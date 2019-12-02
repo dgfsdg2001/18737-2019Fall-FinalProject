83e179f9a4ddb57a94b1e387c30f7533d1be1aa7
Shaomin <shaominc@sv.cmu.edu>
2019-12-02 01:27:46 -0800

    Support afl-gov

--- wpa_supplicant/afl/build.sh
+++ wpa_supplicant/afl/build.sh
@@ -8,12 +8,17 @@ then
 else 
     SRC="wpa_cli_afl_wrapper.c"
     OBJ=${SRC%.c}.o
-    CFLAG="-DMAX_ARGC=10"
+    CFLAG="-DMAX_ARGC=10 -fprofile-arcs -ftest-coverage"
     afl-gcc $CFLAG $SRC -o $OBJ
     if test $1 = "ignore"
     then
         export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
         export AFL_SKIP_CPUFREQ=1
     fi
+
+    rm -rf afl-out/*
+    cp $OBJ ./afl-cov/
+    gnome-terminal -- bash -c 'cd afl-cov; afl-cov -v -d ./../afl-out --live --ignore-core-pattern --sleep 30 --coverage-cmd "./wpa_cli_afl_wrapper.o -f AFL_FILE -a -b -c" --code-dir ./../../;'
+    sleep 2s
     afl-fuzz -i afl-in -o afl-out -- ${PWD}/${OBJ} @@
 fi
