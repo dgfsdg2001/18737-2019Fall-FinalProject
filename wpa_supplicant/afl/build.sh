c70b1b0dd1244961eaca62aaca040371068297da
Shaomin <shaominc@sv.cmu.edu>
2019-12-02 03:02:44 -0800

    Modify afl/build.sh to make arguments more readable
    "build.sh"
      => Build wrapper and wpa_cli with afl-gcc
    
    "build.run"
      => Run afl & afl-cov
    
    "build run ignore"
      => Run afl & afl-cov ignoring recommended settings from afl.

--- wpa_supplicant/afl/build.sh
+++ wpa_supplicant/afl/build.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-if test $1 = "wpa_cli"
+SRC="wpa_cli_afl_wrapper.c"
+OBJ=${SRC%.c}.o
+CFLAG="-DMAX_ARGC=10 -fprofile-arcs -ftest-coverage"
+
+if test $1 = "run"
 then
-    cd ..
-    make clean
-    CC="afl-gcc" make wpa_cli -j8
-else 
-    SRC="wpa_cli_afl_wrapper.c"
-    OBJ=${SRC%.c}.o
-    CFLAG="-DMAX_ARGC=10 -fprofile-arcs -ftest-coverage"
-    afl-gcc $CFLAG $SRC -o $OBJ
-    if test $1 = "ignore"
+    if $2 = "ignore"
     then
         export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
         export AFL_SKIP_CPUFREQ=1
     fi
-
-    rm -rf afl-out/*
+    # ?? afl-cov does not count the coverage of wpa_cli during off-line analysis. Maybe using the execv is the problem for off-line analysis?
     cp $OBJ ./afl-cov/
-    gnome-terminal -- bash -c 'cd afl-cov; afl-cov -v -d ./../afl-out --live --ignore-core-pattern --sleep 30 --coverage-cmd "./wpa_cli_afl_wrapper.o -f AFL_FILE -a -b -c" --code-dir ./../../;'
-    sleep 2s
+    gnome-terminal -- bash -c 'cd afl-cov; afl-cov -v -d ./../afl-out --overwrite --live --ignore-core-pattern --sleep 30 --coverage-cmd "./wpa_cli_afl_wrapper.o -f AFL_FILE -a -b -c" --code-dir ./../../;'
     afl-fuzz -i afl-in -o afl-out -- ${PWD}/${OBJ} @@
+else
+    # Build wrapper with afl-gcc
+    afl-gcc $CFLAG $SRC -o $OBJ
+
+    # Build wpa_cli with afl-gcc
+    cd ..
+    make clean
+    CC="afl-gcc" make wpa_cli -j8
 fi
